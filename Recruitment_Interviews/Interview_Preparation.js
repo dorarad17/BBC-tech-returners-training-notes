c4da658233c6fecac233a057f6573a63e2e1164d
Rebecca French <rlfweb8@gmail.com>
2020-05-02 14:58:37 +0100

    Updates to notes

--- Recruitment_Interviews/Interview_Preparation.js
+++ Recruitment_Interviews/Interview_Preparation.js
@@ -8,7 +8,7 @@ The Solo session
 Drawing some architecture out? James will send over a whiteboarding video. 
 
 The Pair Programming
-It's more about explaining things through, listening, asking questions, interactionw itht he partner. It's mor about that than completing it.
+It's more about explaining things through, listening, asking questions, interaction  with the partner. It's mord about that than completing it.
 It's about the logical mindset, and how you talk things through. How you would take it going forward. 
 
 Preparation for assessment day
@@ -17,26 +17,24 @@ Review notes from course
 Review personal app
 Review group app. 
 Be confident about talking about all of that. they may ask you to talk around your code, why did you do this, why did you do that. 
-They might ask things like 'the bbc has a searchbar, how do you think we do that?'Can talk through the issue. How many people use it? Lots of content. Talking out loud about the issues. And people will be annoyed if it takes 
-They are looking for abstract thinking, not so much the tech option. You're using it on every page of the site, so if you are using react, it makes sense for it to be a component, so you can re-use it. 
+They might ask things like 'the bbc has a searchbar, how do you think we do that?'Can talk through the issue. How many people use it? Lots of content. Talking out loud about the issues i.e. how users will be annoyed if search takes longer than a millisecond.
+They are looking for abstract thinking, not so much the tech option. You're using it on every page of the site, so if you are using React, it makes sense for it to be a component, so you can re-use it. 
 A question a bit like 'how many golf balls are there in an aeroplane' or 'why are there two escalators going up and only one going down'
 
 Have you come across tech and do you know what problems it solves? 
-Have you come across RDS and what problem it solves. If you can't answer it, you could say can you tell me what field it is in and i'll see if I can relate it to smething I have learnt. 
+Have you come across Relational Database Service (RDS) and what problem it solves. If you can't answer it, you could say can you tell me what field it is in and i'll see if I can relate it to something I have learnt. 
 
 Involvement in tech community - meetups etc. 
 personal prjects - things you want to do after the course
-Passion and hearing that you care and that you relaly want to work for the organisation. 
+Passion and hearing that you care and that you really want to work for the organisation. 
 How you keep up to date on tech topics etc. 
 Follow poeple on bbc on twitter. Tech blogs that BBC have written themselves. 
 Women in tech events. 
 
 They're not looking to trip us up, they really want us to come at work at the BBC. They want to know what we have learnt and how to place them well at the BBC. 
 
-
 Presentation - look at a page of the BBC website and talk about how you would change it. 
 Gender balance and racial balance of images
-
 
 1-2-1 Interview
 
@@ -46,28 +44,24 @@ BBC will have core competencies they need you to have. Maybe 6. Communication, i
 "Follow the circle: your role (talk about yourself and what you achieved) the outcome, the challenges (but always reframe them as improvements and back them up with what you learnt and what was overcome)
 Write down an example in advance that covers all of those - examples. You can take in notes with you - looks like you're prepared. 
 
-
-
 Cultural questions
  - example questions from Beckie's 
  Challenge and achivements
  Looking for passion and commitment
  Communication is a skill - why do you believe that is one of your skills, give exact examples. 
  
- What are your devleopment areas? 
+ What are your devlopment areas? 
  You could answer i.e. Time management - using online apps to manage my time. 
  
  Perhaps an area of tech is my weakness - but doing online courses and going to meetups etc. So I'm working on that. 
  
- Where do you see yourself in 5 years time? My priority at the moment is applying what I've learnt over last 15 weeks into a professional setting and continue learning. 
- Then I'll work with my line manager to develop a development plan. 
+ Where do you see yourself in 5 years time? If you can't answer that, you can answer soemrhing like "My priority at the moment is applying what I've learnt over last 15 weeks into a professional setting and continue learning. Then I'll work with my line manager to develop a development plan."
  
  Why do you want to work for us? Have a look at bbc website, things doing in community, have a look at their goals and use that to help you answer that question. 
 
-
 Mind mapping
 Graph gets you to think about key areas you will be asked about in interview
-Your journey into coding: break down into bulletpoints. When your interest in coding started. linking into story about how led into being here. Talk it thorugh. Alex and the woodland app. Jonny. 
+Your journey into coding: break down into bulletpoints. When your interest in coding started. Linking into story about how led into being here. Talk it thorugh. Alex and the Woodland app. Jonny. 
 
 Projects and work experience
 
@@ -90,10 +84,10 @@ flexible working - can turn that into a question?
 development - what development opportunites are there?
 So you can ask the right questions that you need answering so you can decide whether to accept the job. 
 
+
 PREPARE
 
 Do some role playing, practising, in front of people I feel comfy with. 
-
 
 The presentation
 Go through
@@ -113,16 +107,15 @@ James has another project he can send us to do in the interim. Takes concepts wi
 
 ------------
 
-
+Jyoti asked: 
+Just reading through the "Role Specific Capabilities" areas Sam mentioned we'll be asked about at assessment in his email today. Would you be able to explain what is meant by no 10. Domain Expertise in the first attachment? What exactly does domain mean in this context? 
 
 On the Domain Expertise front
 The BBC will know that you wont have BBC problem domain knowledge. EG. You won’t have knowledge of the specific tech they use or processes to ensure that people can vote during strictly. In the context of interview I would assume that they mean domain knowledge within the context of the course.
 In practice, lets take DNS as example they would like to like to know that not only do you know what it is and how it works but also the reason it exists and why it has some of the features described in the course.
-Or another example - React and specifically components, not only to know that they exist, how you write them but also why that logical construct of a “component” exists IE. What problems it solves
+Or another example - React and specifically components, not only to know that they exist, how you write them but also why that logical construct of a “component” exists i.e. What problems it solves
 If you’ve ever come across the ShuHaRi scale - its demonstrating aspects of Shu and Ha
 https://martinfowler.com/bliki/ShuHaRi.html (edited) 
-
-
 
 
 James:school:  19:52
@@ -140,8 +133,19 @@ Thanks
 Sam
 
 
-He also followed up with:
+Sam also followed up with:
 ===
 Oh one more thing - for you to share.
 There is a good set of “test” questions ob BBC Bitseize under KS3 found here: https://www.bbc.co.uk/bitesize/guides/zts8d2p/revision/1
 They might want to have a look at, no need to do the learnings for each section but the test questions give a bit of a clue as to how our questions *might* look - they are not on there as an FYI but the style is similar. (edited) 
+
+===
+
+
+This was going to be our original interview at BC, so new online one may be a bit similar: 
+Then on the day...
+30 minute interview with at least 2 BBC engineers
+30 minute presentation (15 mins duration, 15 mins Q and A) the candidates will be given topic prior to interview
+90 mins - 2 technical exercises 
+One paired with a BBC engineer
+One alone based upon what they have covered in course
---
+++ Thursday_Evening_Talks_April-2020_onwards/Viv Galinari.js
@@ -0,0 +1,161 @@
+/* 
+
+Augmented Reality Showcase
+
+Viv Galinari
+Design & Development
+@vivgalinari
+
+Codeacademy Pro - The Full Stack Path
+
+HTML & CSS
+Learn Sass
+JavaScript
+JQuery
+Angular
+Learn the command line
+Ruby
+Learn Ruby on Rails
+Ruby on Rails: Authentication
+Learn Git
+Python
+
+Brazil - did business at Uni. 
+Not customary for women to do engineering in Brazil
+Masters in Innovation, but don't want to be an Innovation Manager, so decided to self-teach Coding. 
+Spent 2 years teaching herself coding inensively. Went to Asia to have tiny rent and concentrate on coding. 
+Did two months at startup where they didn't nurture and help beginners. 
+First job at the BBC. Amazing, nurturing place.  
+
+Creative Coding Background
+
+JS based libraries for creative coding in the browswer, instant gratification, without needing a backend etc. Same as instagram filters. 
+Processing CodeCircle
+MaxiJS CodeCircle
+A Frame Stitch
+P5 Snake Game
+
+All these things showed the BBC that she can learn and that she has a personal passion for it. 
+
+Investing in things that are instant. 
+
+** Use cases for AR - Augmented Reality
+
+Able to make extra cash by making filters for brands. 
+Facebook Ads, Shopify and Adidas E-com
+Safari uses USDZ file to render AR in the browswer
+Chrome have their own specifications and they might standardise AR for web for the future. 
+
+E-com is big. So if you want to make your career future-proof, that's something to look at. 
+
+** Spark AR - can do visual coding. Everything is reusable. 
+
+You put all your assets on the right side, and everything is a child of teh devide. You drag and drop things into it. 
+Load assets from elsewhere. 
+
+DEVELOPING INSTAGRAM / FACEBOOK EFFECTS
+
+Do all the Spark tutorials
+Official Spark AR Community on Facebook
+Spark AR TV - collection of youtube tutorials
+Josh Beckwith
+Maru Studio
+Catalyst
+WalwaysCodingSomething
+Advanced Rendering Options
+SculptGL
+cotrader - download 3D models
+
+DEVELOPING EFFECTS
+
+1. Develop in Spark AR and App for preview
+2. Publish on Spark AR Hub
+3. Share with friends
+
+CAPABILITIES
+
+Tracking people and places
+then:
+Building Your Scene
+And this is all out of the box. 
+
+MY FILTERS:
+
+Eyes on glasses
+Earrings look like faces
+Multiple eyes
+Fragments
+Medusa
+
+STATS (on Instagram): 
+
+"Weirdest" Top Model edition
+
+BIGGEST LEARNINGS as a freelancer making Instagram filters:
+
+If you want to make any money, you have to go to places and linkedin and go to agencies. 
+Instagram is just to show a portfolio. 
+
+It's worth investing time learning a software that has such a good distribution channel
+
+CREATIVE PROCESS:
+
+Snake on the face
+3D asset that maps onto the face. You can download that from Spark AR. i.e. download face assets here. 
+
+Spark AR - Create New - or use one of the readymade ones. Create a new Blank Project and it opens the new window. 
+Add object, Face Tracker. 
+Add face Mash (this checked material is because tehre is nothing there)
+Can put materials there - default material. Can make it bigger/smaller. 
+Pull in Asset of the snaker her friend drew. 
+Double click on material to change properties of material. 
+Can click on retouch to remove wrinkles! 
+Can click on New Mask and put the snake onto that. 
+And then pull that onto your face and it will move with you. 
+And it makes you a filter
+Can add directional light - colour them
+Can blur the edges of the filter so you don't get hard edge to the mask at the hairline. 
+Use the Patch Editor to bring menus out 
+Can test it by sending to instagram. Take a video of yourself on your phone using the filter. Can publish by using Upload. Has to be max 4MB for instagram on phones. Publish new effect. 
+
+YOu can also do everyting by coding by opening the scripting file and using JavaScript. 
+
+If you have a design background, your filters will look good. If you have engineering background, your filters will be special becuase not many people will know how else to do that. 
+
+The earring one was self-scripted. 
+
+Best to do Spark tutorials first and master the interface and then go into the code and you need to look at documentation more for that. 
+It's React JavaScript. 
+
+Jyoti asked: Is there scope at the BBC to do really experimental creative stuff? 
+This AR stuff is very experimental, so it's nice to have a job that is building a career that is more solid. And make money. Becuase the creative industries expect you to do everything for free. 
+
+Viv did the foundation course on that visual thing. 
+The more you learn, the more fun you have. Becuase you're not stuck on little errors. 
+
+Jyoti asks about Viv's BBC Interview
+Soft skills are underrated. If you can talk well, that really makes a difference. 
+1.5 hours chatting. Everyone is so nice. 
+BBC was easiest out of all the interviews Viv did. They gave Viv an app to implement. It was plain JS and there were a number of things you could choose to implement. 30-40 mins. 
+Then had a chat with Andy and Jess. 
+Then that was it. 
+
+Then went to two other places to do interviews. 
+
+Alex asked: Have you done any generative art in the browser?
+If you want ot make music with code, I shared some libraries. They are JS. Quite small libraries. 
+I go to ogrerave and search for tidal cycle, they make music with code. You can go to their gigs - you go to gig and watch them coding. They make the visuals with code too. Somerset House there is a lot of things you can go to to learn. Hack spaces too. P5. 
+
+creative coding in JS
+Daniel Schiffman - codetrain on youtube
+Will give you something to show
+
+Wesbos - 30 days of JS - more functional things. 
+
+Spark AR is just for face filters. And they have closed their API so you can't add other libraries. 
+
+Things are heading to be more augmented. It's pretty fun and so rewarding. When people think you are a genius but it's just that you try hard. 
+
+When you get a steep learning curve it's hard to get a sense of gratification. So these fun things are to keep you creative and fun. 
+
+                             
